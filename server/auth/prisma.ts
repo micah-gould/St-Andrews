0570bbbe30da8223935c51afdc50b1584f96f7e1
Micah Gould <micah.gould.1@aptiv.com>
2026-04-29 10:55:26 +0000

    Update NPM packages

--- server/auth/prisma.ts
+++ server/auth/prisma.ts
@@ -1,10 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../src/generated/prisma/client";
 
 const globalForPrisma = globalThis as typeof globalThis & {
   __prisma?: PrismaClient;
 };
 
-export const prisma = globalForPrisma.__prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.__prisma ||
+  new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL ?? "",
+    }),
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.__prisma = prisma;
